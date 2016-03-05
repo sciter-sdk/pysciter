bcc8a23ad62f712c7905dd4b4500edc1daf6c05f
pravic <ehysta@gmail.com>
2016-03-05 13:10:11 +0300

    update README and setup.py for PyPi packaging.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Sciter bindings for Python
 
-Sciter is an embeddable [multiplatform](http://sciter.com/sciter/crossplatform/) HTML/CSS/script engine designed to render modern desktop application UI. It's a **compact**, single dll/dylib/so file (4-8 mb), engine without any additional dependencies. 
+Sciter is an embeddable [multiplatform](http://sciter.com/sciter/crossplatform/) HTML/CSS/script engine with GPU accelerated rendering designed to render modern desktop application UI. It's a compact, single dll/dylib/so file (4-8 mb), engine without any additional dependencies.
 
-Sciter supports all standard elements defined in HTML5 specification [with some additions](http://sciter.com/developers/for-web-programmers/). CSS extended to better support Desktop UI development, e.g. flow and flex units, vertical and horizontal alignment, OS theming. 
+Check the [screenshot gallery](https://github.com/oskca/sciter#sciter-desktop-ui-examples) of the desktop UI examples.
+
+Physically Sciter is a mono library which contains:
+
+* [HTML and CSS](http://sciter.com/developers/for-web-programmers/) rendering engine based on the H-SMILE core used in [HTMLayout](http://www.terrainformatica.com/htmlayout/main.whtm),
+* JavaScript alike [Scripting engine](http://sciter.com/developers/sciter-docs/) – core of [TIScript](http://sciter.com/developers/for-web-programmers/tiscript-vs-javascript/) which by itself is based on [c-smile](http://c-smile.sourceforge.net/) engine,
+* Persistent [Database](http://sciter.com/docs/content/script/Storage.htm) (a.k.a. [JSON DB](http://terrainformatica.com/2006/10/what-the-hell-is-that-json-db/)) based on excellent DB products of [Konstantin Knizhnik](http://garret.ru/databases.html).
+* [Graphics](http://sciter.com/docs/content/sciter/Graphics.htm) module using excellent AGG library of Maxim Shemanarev at [www.antigrain.com](http://antigrain.com).
+* Network communication module, it relies on [Libcurl](http://curl.haxx.se/), the Great.
+
+Sciter supports all standard elements defined in HTML5 specification [with some additions](http://sciter.com/developers/for-web-programmers/). CSS extended to better support Desktop UI development, e.g. flow and flex units, vertical and horizontal alignment, OS theming.
 
 [Sciter SDK](http://sciter.com/download/) comes with demo "browser" with builtin DOM inspector, script debugger and documentation browser:
 
--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 try:
     from setuptools import setup
 except ImportError:
@@ -7,37 +10,79 @@ config = {
     'name': 'PySciter',
     'author': 'pravic',
     'author_email': 'ehysta@gmail.com',
-    'description': 'Python bindings for the Sciter - Embeddable HTML/CSS/script engine.',
+    'description': 'Python bindings for the Sciter - Embeddable HTML/CSS/script engine (cross-platform desktop GUI toolkit).',
     'url': 'http://github.com/pravic/pysciter/',
-    'download_url': 'http://github.com/pravic/pysciter/',
+    'download_url': 'https://github.com/pravic/pysciter/releases',
+    'bugtrack_url': 'https://github.com/pravic/pysciter/issues',
     'version': '0.4',
+    'platforms': ['Windows', 'Linux', 'MacOS X', ],
     'packages': ['sciter', 'sciter.capi'],
     'install_requires': [''],
     'scripts': [],
-    'keywords': ['gui', 'sciter', 'tiscript', 'htmlayout', 'html', 'css', 'web', 'cross-platform',],
+    'keywords': ['gui', 'sciter', 'tiscript', 'htmlayout', 'html', 'css', 'web', 'cross-platform', ],
     'license': 'MIT',
     'classifiers': [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows :: Windows XP',
         'Operating System :: Microsoft :: Windows :: Windows Vista',
         'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
         'Environment :: MacOS X',
         'Environment :: Win32 (MS Windows)',
         'Environment :: X11 Applications :: GTK',
+        'Environment :: Web Environment',
+        'Programming Language :: C++',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Desktop Environment',
+        'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
         'Topic :: Software Development :: Widget Sets',
     ],
+    'long_description': """
+Introduction
+============
+Sciter (http://sciter.com) is an embeddable HTML/CSS/script engine with GPU accelerated rendering for desktop application UI.
+It's a compact, single dll/dylib/so file (4-8 mb), engine without any additional dependencies.
+
+Sciter uses Direct2D GPU accelerated graphics on modern Windows versions and GDI+ on XP.
+On OS X, it uses standard CoreGraphics primitives, while the Linux version uses Cairo.
+
+Sciter uses HTML5 set of elements, implements CSS level 2.1 in full, plus the most popular features of CSS level 3.
+It also contains custom CSS extensions that are required to support desktop UI cases.
+For example, flex units and various layout managers.
+
+Check the `screenshot gallery <https://github.com/oskca/sciter#sciter-desktop-ui-examples>`_ of the desktop UI examples.
+
+
+Installation
+============
+
+For installation instructions and usage examples please refer to `github project page <https://github.com/pravic/pysciter#getting-started>`_.
+
+
+Compatibility
+=============
+
+PySciter requires Python 3.x (2.7 is planned in near future).
+
+Sciter works on Microsoft Windows XP and above (x86/x64), Apple OS X v 10.7 and above (64-bit/32-bit) and Linux/GTK (GTK v 3.0 and above, 64-bit only).
+
+Feedback and getting involved
+=============================
+- PySciter Code Repository: https://github.com/pravic/pysciter
+- Issue tracker: https://github.com/pravic/pysciter/issues
+- Sciter official website: http://sciter.com
+- Sciter forum: http://sciter.com/forums/
+- Sciter SDK: https://github.com/c-smile/sciter-sdk
+
+""",
 }
 
 setup(**config)
