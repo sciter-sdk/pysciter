4fe2673e29a9cf143693b3c25726689881df2678
pravic <ehysta@gmail.com>
2018-03-22 18:27:11 +0300

    version 0.4.21

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ config = {
     'url': 'https://github.com/sciter-sdk/pysciter/',
     'download_url': 'https://github.com/sciter-sdk/pysciter/releases',
     'bugtrack_url': 'https://github.com/sciter-sdk/pysciter/issues',
-    'version': '0.4.20',
+    'version': '0.4.21',
     'platforms': ['Windows', 'Linux', 'MacOS X', ],
     'packages': ['sciter', 'sciter.capi'],
     'install_requires': [''],
